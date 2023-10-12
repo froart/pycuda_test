7ac655d094d033376b28f58cdfa863b5c0c1d734
froart <artemiusxyz@gmail.com>
2023-10-12 05:30:07 +0200

    Added CPU version

--- code.py
+++ code.py
@@ -2,12 +2,13 @@ import pycuda.driver as cuda
 import pycuda.autoinit
 import pycuda.compiler as compiler
 import numpy as np
+import datetime as dt
 
 # CUDA kernel function
 source_code = """
-__global__ void my_kernel(float *data) {
+__global__ void my_kernel(float *data_in, float *data_out) {
     int idx = threadIdx.x + blockIdx.x * blockDim.x;
-    data[idx] *= 2.0; // Example: double each element
+    data_out[idx] = 2.0 * data_in[idx]; // Example: double each element
 }
 """
 
@@ -16,22 +17,42 @@ module = compiler.SourceModule(source_code)
 my_kernel = module.get_function("my_kernel")
 
 # Data on the CPU
-data_cpu = np.random.rand(100).astype(np.float32)
+data_cpu_in  = np.random.rand(100).astype(np.float32)
+print("Generated random array... ", data_cpu_in)
+data_cpu_out = [] 
 
-# print initial data
-print(data_cpu)
+# CPU version
+print("Beginning the execution of the CPU version...")
+time_start   = dt.datetime.now()
 
+for element in data_cpu_in:
+    data_cpu_out.append(2 * element)
+
+time_end     = dt.datetime.now()
+time_elapsed = time_end - time_start
+print("Done...")
+print("Output array is: ", data_cpu_out)
+print("Execution time: ", time_elapsed.microseconds, " mcs")
+
+print("Beginning the execution of the GPU version...")
+time_start   = dt.datetime.now()
 # Allocate GPU memory
-data_gpu = cuda.mem_alloc(data_cpu.nbytes)
+data_gpu_in  = cuda.mem_alloc(data_cpu_in.nbytes)
+data_gpu_out = cuda.mem_alloc(data_cpu_in.nbytes)
 
 # Copy data from CPU to GPU
-cuda.memcpy_htod(data_gpu, data_cpu)
+cuda.memcpy_htod(data_gpu_in, data_cpu_in)
 
 # Launch the CUDA kernel
-my_kernel(data_gpu, block=(100, 1, 1), grid=(1, 1))
+my_kernel(data_gpu_in, data_gpu_out, block=(100, 1, 1), grid=(1, 1))
 
 # Copy data back from GPU to CPU
-cuda.memcpy_dtoh(data_cpu, data_gpu)
+cuda.memcpy_dtoh(data_cpu_out, data_gpu_out)
 
-# Data is now modified on the CPU
-print(data_cpu)
+time_end     = dt.datetime.now()
+time_elapsed = time_end - time_start
+print("Done...")
+print("Output array is: ", data_cpu_out)
+print("Execution time: ", time_elapsed.microseconds, " mcs")
+
+print("Exiting...")
