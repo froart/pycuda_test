6fd5842b587ebb3f3668196b33602db812253b84
froart <artemiusxyz@gmail.com>
2023-10-12 05:45:40 +0200

    Number of elements is now passed as an argument.

--- code.py
+++ code.py
@@ -3,6 +3,7 @@ import pycuda.autoinit
 import pycuda.compiler as compiler
 import numpy as np
 import datetime as dt
+import sys
 
 # CUDA kernel function
 source_code = """
@@ -12,12 +13,14 @@ __global__ void my_kernel(float *data_in, float *data_out) {
 }
 """
 
+nelements = int(sys.argv[1]) 
+
 # Compile the CUDA module
 module = compiler.SourceModule(source_code)
 my_kernel = module.get_function("my_kernel")
 
 # Data on the CPU
-data_cpu_in  = np.random.rand(100).astype(np.float32)
+data_cpu_in  = np.random.rand(nelements).astype(np.float32)
 print("Generated random array... ", data_cpu_in)
 data_cpu_out = [] 
 
@@ -37,7 +40,7 @@ print("Execution time: ", time_elapsed.microseconds, " mcs")
 print("Beginning the execution of the GPU version...")
 time_start   = dt.datetime.now()
 
-data_cpu_out = np.zeros(100).astype(np.float32)
+data_cpu_out = np.zeros(nelements).astype(np.float32)
 # Allocate GPU memory
 data_gpu_in  = cuda.mem_alloc(data_cpu_in.nbytes)
 data_gpu_out = cuda.mem_alloc(data_cpu_in.nbytes)
@@ -46,7 +49,7 @@ data_gpu_out = cuda.mem_alloc(data_cpu_in.nbytes)
 cuda.memcpy_htod(data_gpu_in, data_cpu_in)
 
 # Launch the CUDA kernel
-my_kernel(data_gpu_in, data_gpu_out, block=(100, 1, 1), grid=(1, 1))
+my_kernel(data_gpu_in, data_gpu_out, block=(nelements, 1, 1), grid=(1, 1))
 
 # Copy data back from GPU to CPU
 cuda.memcpy_dtoh(data_cpu_out, data_gpu_out)
